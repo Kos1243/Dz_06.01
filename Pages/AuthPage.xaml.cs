0eaaa696df8b59f546bc971c1fd472e5d8c5c340
Kirill Murzin <54834040+Kos1243@users.noreply.github.com>
2024-02-29 09:31:33 +0300

    Add files via upload

---
+++ Pages/AuthPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Entity;
+using Wpf.Classes;
+
+
+namespace Wpf.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+        private void BtnMoveFirst_OnClick(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(content: new FirstPage());
+        }
+        private void BtnMoveFirs_OnClick(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(content: new Page3());
+        }
+        private void BtnMoveFir_OnClick(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(content: new ClientPage());
+        }
+        private void BtnMoveFi_OnClick(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(content: new Page4());
+        }
+        private void BtnMoveF_OnClick(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(content: new Page5());
+        }
+    }
+}
+
+
+
+        
+    
+
---
+++ Pages/Page4.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Entity;
+using Wpf.Classes;
+
+namespace Wpf.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Page4.xaml
+    /// </summary>
+    public partial class Page4 : Page
+    {
+        public Page4()
+        {
+            InitializeComponent();
+            DGridProducts.ItemsSource = Entities.GetContext().Recipe.ToList();
+        }
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AuthPage());
+        }
+        
+        }
+    }
